fd5d9cb2aeb9fa5534833a383d957f76f390eeec
Aleksas-7 <aleksas.grinys@gmail.com>
2024-01-15 19:31:11 +0200

    start9

--- index.js
+++ index.js
@@ -4,23 +4,33 @@ let card = {
     id: "",
     faceImagePath: "",
     backImagePath: "",
+    faceShown: false,
 
-    showValueFace : function() {
-        $("#" + this.id).show();
-    },
-    hideValueFace : function() {
-        $("#" + this.id).hide();
+    switchCardFace : function() {
+        let cardFace = "#" + this.id + " img:nth-child(1)";
+        let cardBack = "#" + this.id + " img:nth-child(2)";
+
+        if (this.faceShown) {            
+            $(cardFace).hide();
+            $(cardBack).show();
+        } else {
+            $(cardFace).hide();
+            $(cardBack).show();
+        }
+
+        this.faceShown = !this.faceShown;
     }
 }
 
+let cardDeck = [];
 
 function makeDeck () {
-    let cardDeck = [];
+    
     for (let i = 1 ; i <= 10 ; i++){
         let madeCard = {...card};
-        madeCard["id"] = String(i);
-        madeCard["value"] = "club" + madeCard["id"];
-        madeCard["faceImagePath"] = "art/clubs" + madeCard["id"] + ".png";
+        madeCard["id"] = "club" + String(i);
+        madeCard["value"] = "club" + String(i);
+        madeCard["faceImagePath"] = "art/clubs" + String(i) + ".png";
         madeCard["backImagePath"] = "art/card_back.png";
         cardDeck.push(madeCard)
     }
@@ -34,9 +44,22 @@ $(document).ready(function() {
 
 
 $(".card").on("click", function(){
-    $(this).hide();
+    $(this).switchCardFace();
 });
 
 makeDeck();
+
+function renderDeck() {
+    for (let card of cardDeck) {
+        var CardDiv = $("<div>");
+        CardDiv.attr("id", card.id);
+        CardDiv.attr("class", "card");
+        CardDiv.append("<img class=\"face\" src=\"" + card.faceImagePath + "\">");
+        CardDiv.append("<img class=\"back\" src=\"" + card.backImagePath + "\">");
+        $("#game-screen").append(CardDiv);
+    }
+}
+
+renderDeck();
 
 });
