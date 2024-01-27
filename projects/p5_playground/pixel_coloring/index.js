33d0b52cf73b89da1614ab2cae0acf592b20df77
Aleksas-7 <aleksas.grinys@gmail.com>
2024-01-27 16:43:17 +0200

    24

--- projects/p5_playground/pixel_coloring/index.js
+++ projects/p5_playground/pixel_coloring/index.js
@@ -1,99 +1,39 @@
 
-let cols, rows;
-let grid;
-let cellWidth = 10;
-let currentColor = [0, 200, 0, 255];
-let backgroundColor = [0, 0, 0, 125];
 
-let gridShow = true;
-let gridColor = [25, 25, 25];
 
-let totalCellLimit = true;
-let totalCellCount = 16;
+function hexToRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? [
+        parseInt(result[1], 16),
+        parseInt(result[2], 16),
+        parseInt(result[3], 16), 255
+     ] : null;
+} 
 
-function make2DArray(c, r){
-    let arr = new Array(c);
-    for (let i = 0 ; i < arr.length ; i++){
-        arr[i] = new Array(r);
-        for(let j = 0 ; j < r ; j++){
-            arr[i][j] = new Array(4);
-        }
-    }
-    return arr;
+function updateBGAcolor(){
+    var bgAlpha = parseInt($("#BGAlpha").val());
+    $("#BGAshow").html("(" + bgAlpha + ")");
+    backgroundColor[3] = bgAlpha;
 }
 
-function colorCell() {
-    let col = floor(mouseX / cellWidth);
-    let row = floor(mouseY / cellWidth);
-    if (mouseButton === LEFT){
-        grid[col][row] = currentColor;
-    }
-    else if (mouseButton === RIGHT) {
-        grid[col][row] = backgroundColor;
-    }
-}
+$(document).ready(function () {
+    $("#submitOptions").click(function (){
+        
+    });
+    $("#BGcol").on("input", function (){
+        backgroundColor = hexToRgb(this.value);
+        updateBGAcolor();
+    });
 
+    $("#BGAlpha").on("input", function (){
+        updateBGAcolor();
+    });
+    $("#gridShow").on("input", function (){
+        console.log(this.value);
+        gridShow = !gridShow;
+    });
 
-
-
-function setup() {
-    colorMode(RGB);
-    createCanvas(400, 400);
-    if (totalCellLimit){
-        cellWidth = floor(width / totalCellCount);
-        cols = totalCellCount;
-        rows = totalCellCount;
-    } else {
-        cols = width / cellWidth;
-        rows = height / cellWidth;
-    }
-    grid = make2DArray(cols, rows);
-    for(let i = 0 ; i < cols ; i++){
-        for(let j = 0 ; j < rows ; j++){
-            grid[i][j] = backgroundColor;
-        }
-    }
-    
-}
-
-function mousePressed(){
-    colorCell();
-}
-function mouseDragged(){
-    colorCell();
-}
-  
-function draw() {
-    background(backgroundColor);
-    if (totalCellLimit){
-        for(let i = 0 ; i < totalCellCount ; i++){
-            for(let j = 0 ; j < totalCellCount ; j++){
-                if (!gridShow){
-                    strokeWeight(0);
-                } else {
-                    strokeWeight(1);
-                }
-                stroke(gridColor);
-                fill(grid[i][j]);
-                let x = i * cellWidth;
-                let y = j * cellWidth;
-                square(x, y, cellWidth);
-            }
-        }
-    } else {
-        for(let i = 0 ; i < cols ; i++){
-            for(let j = 0 ; j < rows ; j++){
-                if (!gridShow){
-                    strokeWeight(0);
-                } else {
-                    strokeWeight(1);
-                }
-                stroke(gridColor);
-                fill(grid[i][j]);
-                let x = i * cellWidth;
-                let y = j * cellWidth;
-                square(x, y, cellWidth);
-            }
-        }
-    }
-}
+    $("#brushColor").on("input", function (){
+        currentColor = hexToRgb(this.value);
+    });
+});
---
+++ projects/p5_playground/pixel_coloring/pFive.js
@@ -0,0 +1,99 @@
+
+let cols, rows;
+let grid;
+let cellWidth = 10;
+let currentColor = [0, 200, 0, 255];
+let backgroundColor = [0, 0, 0, 255];
+
+let gridShow = true;
+let gridColor = [25, 25, 25];
+
+let totalCellLimit = true;
+let totalCellCount = 16;
+
+function make2DArray(c, r){
+    let arr = new Array(c);
+    for (let i = 0 ; i < arr.length ; i++){
+        arr[i] = new Array(r);
+        for(let j = 0 ; j < r ; j++){
+            arr[i][j] = new Array(4);
+        }
+    }
+    return arr;
+}
+
+function colorCell() {
+    let col = floor(mouseX / cellWidth);
+    let row = floor(mouseY / cellWidth);
+    if (mouseButton === LEFT){
+        grid[col][row] = currentColor;
+    }
+    else if (mouseButton === RIGHT) {
+        grid[col][row] = backgroundColor;
+    }
+}
+
+
+
+
+function setup() {
+    colorMode(RGB, 255, 255, 255, 100);
+    createCanvas(400, 400).parent(select("#canvasContainer"));
+    if (totalCellLimit){
+        cellWidth = floor(width / totalCellCount);
+        cols = totalCellCount;
+        rows = totalCellCount;
+    } else {
+        cols = width / cellWidth;
+        rows = height / cellWidth;
+    }
+    grid = make2DArray(cols, rows);
+    for(let i = 0 ; i < cols ; i++){
+        for(let j = 0 ; j < rows ; j++){
+            grid[i][j] = backgroundColor;
+        }
+    }
+    
+}
+
+function mousePressed(){
+    colorCell();
+}
+function mouseDragged(){
+    colorCell();
+}
+  
+function draw() {
+    background(backgroundColor);
+    if (totalCellLimit){
+        for(let i = 0 ; i < totalCellCount ; i++){
+            for(let j = 0 ; j < totalCellCount ; j++){
+                if (!gridShow){
+                    strokeWeight(0);
+                } else {
+                    strokeWeight(1);
+                }
+                stroke(gridColor);
+                fill(grid[i][j]);
+                let x = i * cellWidth;
+                let y = j * cellWidth;
+                square(x, y, cellWidth);
+            }
+        }
+    } else {
+        for(let i = 0 ; i < cols ; i++){
+            for(let j = 0 ; j < rows ; j++){
+                if (!gridShow){
+                    strokeWeight(0);
+                } else {
+                    strokeWeight(1);
+                }
+                stroke(gridColor);
+                fill(grid[i][j]);
+                let x = i * cellWidth;
+                let y = j * cellWidth;
+                square(x, y, cellWidth);
+            }
+        }
+    }
+}
